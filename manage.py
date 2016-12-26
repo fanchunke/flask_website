b0c0696167032aea8236853bb6973c6905063ac2
fanchunke1991 <fanchunke1991@163.com>
2016-12-26 22:26:05 +0800

    增加migrate和shel的命令行命令

--- manage.py
+++ manage.py
@@ -1,9 +1,22 @@
 # -*- coding:utf-8 -*-
 
 import os
-from ChiBurger import create_app
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Shell, Manager
 
+from ChiBurger import create_app, db
+
+# create app instance
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
+def make_shell_context():
+	return dict(app=app, db=db)
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('shell', Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
 if __name__ == '__main__':
-    app.run()
+    manager.run()
