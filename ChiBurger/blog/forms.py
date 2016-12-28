887754a5287258a604019ba11feaa6ada29e077a
fanchunke1991 <fanchunke1991@163.com>
2016-12-28 12:35:13 +0800

    增加登陆表单和登录视图

---
+++ ChiBurger/blog/forms.py
@@ -0,0 +1,8 @@
+from flask.ext.wtf import Form
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Required, Email
+
+class LoginForm(Form):
+    email = StringField('Email', validators=[Required(), Email()])
+    password = PasswordField('Password', validators=[Required()])
+    submit = SubmitField('Log in')
--- ChiBurger/blog/views.py
+++ ChiBurger/blog/views.py
@@ -1,7 +1,33 @@
 # -*- coding:utf-8 -*-
 
-from . import blog
+from flask import render_template, url_for, abort, redirect, flash, get_flashed_messages
+from flask_login import login_user
 
-@blog.route('/')
+from . import blog
+from ..models import Article, User
+from .forms import LoginForm
+
+@blog.route('/', methods=['GET'])
 def index():
-    return "This is blog'index page!"
+    articles = Article.query.all()
+    return render_template('blog/index.html', articles=articles)
+
+@blog.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None:
+            if user.vertify_password(form.password.data):
+                login_user(user)
+                flash("You were logged in successfully!")
+
+                next = request.get.args('get')
+                if not is_safe_url(next):
+                    return abort(400)
+                return redirect(next or url_for('blog.index'))
+            flash('Invalid password')
+        flash('Invalid email')
+    return render_template('blog/login.html',form=form)
+
